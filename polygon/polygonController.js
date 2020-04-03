3a1183bda4366675aa73db99355ec80b0179e88d
Mohammad Seyfayi <madsams024@gmail.com>
2020-04-03 21:46:15 +0430

    addPolygon: 200 response : text => json

--- polygon/polygonController.js
+++ polygon/polygonController.js
@@ -4,7 +4,7 @@ const addPolygon = (req, res) => {
     const data = req.body;
     service
         .store(data)
-        .then(data => res.status(200).send(`"${data.properties.name}" successfully added`))
+        .then(data => res.status(200).send({message: `'${data.properties.name}' successfully added`}))
         .catch(error => {
             const status = error.status || 500;
             const message = error.body || error;
